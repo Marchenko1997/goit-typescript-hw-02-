9e377f9ef6b31af06227c7d67e49dd2e733437cf
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-03-22 14:38:42 +0100

    Adding loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,34 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import SearchBar from './SearchBar/SearchBar';
 import { useState } from "react";
 import { fetchImages } from './articles-api';
+import Loader  from './Loader/Loader';
+
 
 
 
 function App() {
   const [images, setImages] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async (topic) => {
     try {
       setImages([]);
       setError(null);
+      setLoading(true);
       const fetchedImages = await fetchImages(topic);
       setImages(fetchedImages);
     } catch (error) {
       setError(error);
+    }finally {
+      setLoading(false);
     }
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSearch} />
+      {loading && <Loader />}
       {error && <p>Error: {error.message}</p>}
       <ImageGallery images={images} />
     </div>
--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,11 +1,21 @@
-
-import Loader from "react-loader-spinner"; 
+import { Audio } from 'react-loader-spinner';
+import css from './Loader.module.css'
 
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className={css.loaderwrapper}>
+      <Audio
+        height={80}
+        width={80}
+        radius={9}
+        color="green"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
